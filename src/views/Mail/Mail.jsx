238feaf2df1c37d764b9cbab651e8970dd3eb30f
Hieu <tranhieu14310@gmail.com>
2021-08-23 23:23:41 +0700

    add more type email

--- src/views/Mail/Mail.jsx
+++ src/views/Mail/Mail.jsx
@@ -8,10 +8,15 @@ require("dotenv").config();
 export default function Mail() {
   const [editable, setEditable] = useState(true);
   const [notifySuccess, setNotifySuccess] = useState(false);
-  const { handleSubmit, register } = useForm();
+  const [fieldsByTypeEmail, setFieldsByTypeEmail] = useState(1);
+  const { handleSubmit, register, reset } = useForm();
   const sendMail = async (body) => {
     try {
       setEditable(false);
+      body.typeEmail = parseInt(body.typeEmail);
+      if (body.typeEmail === 1) {
+        body.number = parseInt(body.number);
+      }
       console.log(body);
       await MailApi.sendMail(body);
       setNotifySuccess(true);
@@ -50,39 +55,39 @@ export default function Mail() {
                 </div>
               </div>
               <div className="form-group row">
-                <label htmlFor="passwordSender" className="col-lg-4 col-form-label">
+                <label htmlFor="passwordSender" className="col-lg-5 col-form-label">
                   Password Sender
                 </label>
                 <div className="">
                   <input type="password" className="form-control" id="email" {...register("passwordSender")} placeholder="Mật khẩu sender" disabled={!editable} />
                 </div>
               </div>
+              <div className="form-group row">
+                <label htmlFor="passwordSender" className="col-lg-4 col-form-label">
+                  Type Email
+                </label>
+                <div className="">
+                  <select
+                    className="form-control custom-select"
+                    {...register("typeEmail")}
+                    onChange={(e) => {
+                      if (parseInt(e.target.value) === 3) {
+                        setFieldsByTypeEmail(3);
+                      } else {
+                        setFieldsByTypeEmail(1);
+                      }
+                      reset({
+                        typeEmail: parseInt(e.target.value),
+                      });
+                    }}
+                  >
+                    <option value="1">Spam Email</option>
+                    <option value="3">Last Chance</option>
+                  </select>
+                </div>
+              </div>
             </div>
             <div className="col-lg-6">
-              <div className="form-group row">
-                <label htmlFor="email" className="col-lg-4 col-form-label">
-                  Email
-                </label>
-                <div className="">
-                  <input type="email" className="form-control" id="email" {...register("emailReceived")} placeholder="Địa chỉ email gửi tới" disabled={!editable} />
-                </div>
-              </div>
-              <div className="form-group row">
-                <label htmlFor="fromEmail" className="col-lg-4 col-form-label">
-                  From Email
-                </label>
-                <div className="">
-                  <input type="email" className="form-control" id="fromEmail" {...register("fromEmail")} placeholder="Email" disabled={!editable} />
-                </div>
-              </div>
-              <div className="form-group row">
-                <label htmlFor="toEmail" className="col-lg-4 col-form-label">
-                  To Email
-                </label>
-                <div className="">
-                  <input type="email" className="form-control" id="toEmail" {...register("toEmail")} placeholder="Email" disabled={!editable} />
-                </div>
-              </div>
               <div className="form-group row">
                 <label htmlFor="toEmail" className="col-lg-4 col-form-label">
                   Name
@@ -92,6 +97,34 @@ export default function Mail() {
                 </div>
               </div>
               <div className="form-group row">
+                <label htmlFor="email" className="col-lg-4 col-form-label">
+                  Email
+                </label>
+                <div className="">
+                  <input type="email" className="form-control" id="email" {...register("emailReceived")} placeholder="Địa chỉ email gửi tới" disabled={!editable} />
+                </div>
+              </div>
+              {fieldsByTypeEmail === 1 && (
+                <>
+                  <div className="form-group row">
+                    <label htmlFor="fromEmail" className="col-lg-4 col-form-label">
+                      From Email
+                    </label>
+                    <div className="">
+                      <input type="email" className="form-control" id="fromEmail" {...register("fromEmail")} placeholder="Email" disabled={!editable} />
+                    </div>
+                  </div>
+                  <div className="form-group row">
+                    <label htmlFor="toEmail" className="col-lg-4 col-form-label">
+                      To Email
+                    </label>
+                    <div className="">
+                      <input type="email" className="form-control" id="toEmail" {...register("toEmail")} placeholder="Email" disabled={!editable} />
+                    </div>
+                  </div>
+                </>
+              )}
+              <div className="form-group row">
                 <label htmlFor="password" className="col-lg-4 col-form-label">
                   Number
                 </label>
@@ -99,40 +132,6 @@ export default function Mail() {
                   <input type="number" className="form-control" id="password" {...register("number")} placeholder="Số lượng mail sẽ gửi" disabled={!editable} />
                 </div>
               </div>
-              {/* <div className="form-group row">
-                <label
-                  htmlFor="confirmPassword"
-                  className="col-lg-4 col-form-label"
-                >
-                  Subject
-                </label>
-                <div className="">
-                  <input
-                    type="text"
-                    className="form-control"
-                    id="confirmPassword"
-                    name="subject"
-                    placeholder="Tiêu đề mail"
-                  ></input>
-                </div>
-              </div> */}
-              {/* <div className="form-group row">
-                <label
-                  htmlFor="confirmPassword"
-                  className="col-lg-4 col-form-label"
-                >
-                  Text
-                </label>
-                <div className="">
-                  <textarea
-                    type="password"
-                    className="form-control"
-                    id="confirmPassword"
-                    name="text"
-                    placeholder="Nội dung mail"
-                  ></textarea>
-                </div>
-              </div> */}
               <div className="form-group row">
                 <label htmlFor="" className="col-lg-4 col-form-label"></label>
                 <div className="col-sm-10">
